fe9b8877fe7150d490a2ea86aeb7d8d24fa7c0e3
pravdomil <pravdomil.toman@gmail.com>
2017-12-10 02:35:44 +0100

    fix method names

--- src/index.ts
+++ src/index.ts
@@ -425,7 +425,7 @@ function generate(definitions: Definition[]) {
                     const methodName = generateFixParamName(param.name);
                     const desc = param.desc.join(" ").trim();
                     if (desc) {
-                        output += "\t * @param " + param.name + " " + desc + "\n";
+                        output += "\t * @param " + methodName + " " + desc + "\n";
                     }
                     return methodName + (param.optional ? "?" : "") + ": " + generateType(param.types);
                 });
